05008ccfdd44c14615c1d50cb56956d367962dde
MeSeurus <redoreole@gmail.com>
2022-06-01 10:22:06 +0300

    Set. First Iteration

---
+++ src/main/java/ru/seurus/idioma/entity/set/SetTwo.java
@@ -0,0 +1,35 @@
+package ru.seurus.idioma.entity.set;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@Table(name = "SET_TWO")
+public class SetTwo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "QUANTITY")
+    private Double quantity;
+
+    @Column(name = "TABLE_NAME")
+    private String tableName;
+
+    @Column(name = "RECIPE_ID")
+    private Integer recipeId;
+
+}
